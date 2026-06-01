5026af3ba92c21065b5d440d0e98a18e7a5bd27d
princek <princesamuelpks@gmail>
2026-06-01 02:10:41 +0000

    Create service-program.api.ts

---
+++ src/features/service-program/service-program.api.ts
@@ -0,0 +1,38 @@
+import { localRepository } from "../../lib/localRepository";
+import { tryInvokeCommand } from "../../lib/tauri";
+import type {
+  ServiceItem,
+  ServiceItemInput,
+  ServiceProgram,
+  ServiceProgramInput
+} from "../../types/service-program";
+
+export async function getActiveServiceProgram() {
+  return tryInvokeCommand<ServiceProgram | null>(
+    "get_active_service_program",
+    undefined,
+    () => localRepository.getActiveServiceProgram()
+  );
+}
+
+export async function listServicePrograms() {
+  return tryInvokeCommand<ServiceProgram[]>("list_service_programs", undefined, () => localRepository.listServicePrograms());
+}
+
+export async function createServiceProgram(input: ServiceProgramInput) {
+  return tryInvokeCommand<ServiceProgram>("create_service_program", { input }, () =>
+    localRepository.createServiceProgram(input)
+  );
+}
+
+export async function addServiceItem(input: ServiceItemInput) {
+  return tryInvokeCommand<ServiceItem>("add_service_item", { input }, () => localRepository.addServiceItem(input));
+}
+
+export async function reorderServiceItems(serviceProgramId: number, itemIds: number[]) {
+  return tryInvokeCommand<ServiceItem[]>(
+    "reorder_service_items",
+    { serviceProgramId, itemIds },
+    () => localRepository.reorderServiceItems(serviceProgramId, itemIds)
+  );
+}
