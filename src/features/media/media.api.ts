74499c00afdc59a37590377348812bd70f22330f
princek <princesamuelpks@gmail>
2026-06-01 02:10:17 +0000

    Create media.api.ts

---
+++ src/features/media/media.api.ts
@@ -0,0 +1,13 @@
+import { localRepository } from "../../lib/localRepository";
+import { tryInvokeCommand } from "../../lib/tauri";
+import type { MediaAsset } from "../../types/media";
+
+export async function listMediaAssets() {
+  return tryInvokeCommand<MediaAsset[]>("list_media_assets", undefined, () => localRepository.listMediaAssets());
+}
+
+export async function importMediaAsset(sourcePath: string) {
+  return tryInvokeCommand<MediaAsset>("import_media_asset", { sourcePath }, () => {
+    throw new Error("Media import is available in the desktop app.");
+  });
+}
